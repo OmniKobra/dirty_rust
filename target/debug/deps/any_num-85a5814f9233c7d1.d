56a90fc87f8c9e7cf6f340673084b504fd990dc4
OmniKobra <omnifyofficial@yahoo.com>
2025-09-21 19:51:00 +0300

    Initialize workspace with any_num crate

---
+++ target/debug/deps/any_num-85a5814f9233c7d1.d
@@ -0,0 +1,7 @@
+C:\Users\Dell\Desktop\dirty_rust\target\debug\deps\any_num-85a5814f9233c7d1.exe: any_num\src\lib.rs any_num\src\num.rs any_num\src\tests.rs
+
+C:\Users\Dell\Desktop\dirty_rust\target\debug\deps\any_num-85a5814f9233c7d1.d: any_num\src\lib.rs any_num\src\num.rs any_num\src\tests.rs
+
+any_num\src\lib.rs:
+any_num\src\num.rs:
+any_num\src\tests.rs:
---
+++ target/debug/deps/any_num-b425aa1119656aea.d
@@ -0,0 +1,7 @@
+C:\Users\Dell\Desktop\dirty_rust\target\debug\deps\libany_num-b425aa1119656aea.rmeta: any_num\src\lib.rs any_num\src\num.rs any_num\src\tests.rs
+
+C:\Users\Dell\Desktop\dirty_rust\target\debug\deps\any_num-b425aa1119656aea.d: any_num\src\lib.rs any_num\src\num.rs any_num\src\tests.rs
+
+any_num\src\lib.rs:
+any_num\src\num.rs:
+any_num\src\tests.rs:
